316e690ffbcf94ec19d791e32205c33e80e49442
AdvancingHermit <88720725+AdvancingHermit@users.noreply.github.com>
2024-09-11 15:11:14 +0200

    Works

--- cbmp.h
+++ cbmp.h
@@ -1,8 +1,12 @@
-//
-// Created by chris on 11/09/2024.
-//
+#ifndef CBMP_CBMP_H
+#define CBMP_CBMP_H
 
-#ifndef CBMP_H
-#define CBMP_H
+#define BMP_WIDTH 950
+#define BMP_HEIGTH 950
+#define BMP_CHANNELS 3
 
-#endif //CBMP_H
+// Public function declarations
+void read_bitmap(char * input_file_path, unsigned char output_image_array[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]);
+void write_bitmap(unsigned char input_image_array[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], char * output_file_path);
+
+#endif // CBMP_CBMP_H
