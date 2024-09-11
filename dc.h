26d7baed0a05a10d15b58d68357a0f462107349b
AdvancingHermit <88720725+AdvancingHermit@users.noreply.github.com>
2024-09-11 16:08:27 +0200

    T2 done

---
+++ dc.h
@@ -0,0 +1,8 @@
+#define CAPTURE_AREA 12
+#include "cbmp.h"
+
+void greyscale(unsigned char *input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){}
+void erodeImage(unsigned char *gs_image[BMP_WIDTH][BMP_HEIGTH]){} //gs = greyscaled.
+char isCell(unsigned char *cap_area[CAPTURE_AREA][CAPTURE_AREA]){} // Is coordinate an image
+void outputImage(){} // Used at the end of detect cells, to output image with crosses
+void detectCells(unsigned char *gs_image[BMP_WIDTH][BMP_HEIGTH]){}
